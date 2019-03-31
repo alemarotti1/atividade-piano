d79955046372255aa528d5db2368f85e54f35da6
Lucas Barros <lbam@cin.ufpe.br>
2019-03-31 19:53:06 -0300

    Adiciona base para a atividade

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    "mode": "development",
+    "entry": ["./src/app.js"],
+    "output": {
+        "path": __dirname+'/dist',
+        "filename": "[name].js"
+    },
+    "module": {
+        "rules": [
+            {
+                "test": /\.js$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "@babel/preset-env"
+                        ],
+                        "plugins": ["@babel/plugin-proposal-class-properties"]
+                    }
+                },
+            },
+            {
+                test: /\.css$/,
+                use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    "plugins": [
+        new MiniCssExtractPlugin({
+            filename: 'index.[hash:8].css',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Custom template',
+            template: 'src/index.html'
+        }),
+    ]
+}
